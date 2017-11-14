c90c1ef5d6aa6a9515ecfb8f493839575d97117f
minsol <minsol@DIRMINDAUGAS>
2017-11-14 19:35:52 +0200

    Add project files.

---
+++ 1-1 uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "Mindaugas";
+            var pavarde = "Sologubas";
+            var amzius = 37;
+            var ugis = 1.85f; //KOMENTARAS - metrais matuojama
+            var svoris = 99; //kg
+            var aukstoji_mokykla = "kazkokia";
+            var akademines_grupe = 'A';
+            var kursas = 3;
+            var studiju_programa = "pati geriausia";
+            var kreditai = 46;
+            
+        }
+    }
+}
