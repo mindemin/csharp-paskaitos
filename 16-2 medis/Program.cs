028b54b77b58db49d1449e34dc49f61a88cd7744
mindemin <minsol@snaige.lt>
2017-11-22 21:31:08 +0200

    kl

---
+++ 16-2 medis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_2_medis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var medis = new medis("egle", 200, 32);
+            medis.isvedimas();
+
+            medis.AmziusDienomis();
+            medis.RaidesZodyjeMedis();
+
+            Console.ReadLine();
+        }
+        // metus isversti i dienas
+        
+    }
+
+}
---
+++ 16-2 medis/medis.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_2_medis
+{
+    class medis
+    {
+        // kintamieji
+        public string Rusis { get; private set; }
+        public int Amzius { get; private set; }
+        public int Aukstis { get; private set; }
+
+
+        // konstruktorius
+        public medis (string rusis, int amzius, int aukstis)
+        {
+            Rusis = rusis;
+            Amzius = amzius;
+            Aukstis = aukstis;
+        }
+
+        // metodai
+        // isvedimas
+        public void isvedimas()
+        {
+            Console.WriteLine("Medis {0}, kurio amzius {1} m., yra {2} m. aukscio", Rusis,Amzius,Aukstis);
+            Console.WriteLine();
+        }
+
+        public void AmziusDienomis()
+        {
+            Console.WriteLine("Medzio amzius dienomis: " + Amzius * 365);
+        }
+        public void RaidesZodyjeMedis()
+        {
+            int ilgis = Rusis.Length;
+            Console.WriteLine("Medzio pavadinimo ilgis - {0} raides", ilgis);
+        }
+    }
+}
