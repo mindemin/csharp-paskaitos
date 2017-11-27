88ee66168e8c2a63172f6b7fde1f75092a1b21ac
mindemin <minsol@snaige.lt>
2017-11-27 18:23:43 +0200

    k

--- projektas/Program.cs
+++ projektas/Program.cs
@@ -17,12 +17,18 @@ namespace projektas
                 new PirmasSaltinis(2, "geria plaukia"),
                 new PirmasSaltinis(3, "medziotojas"),
                 new PirmasSaltinis(4, "visai blogai..."),
-                new PirmasSaltinis(5, "tai apsispresk")
+                new PirmasSaltinis(5, "apsispresk")
             };
 
             Console.WriteLine("Iveskite savo zenkla: ");
             Console.WriteLine(" 1 - Avinas, 2 - Zuvis, 3 - Liutas, 4 - Vezys, 5 - Nezinau");
             var Zenklas = Convert.ToInt32(Console.ReadLine());
+
+            if (Zenklas<=0 || Zenklas >5)
+            {
+                Zenklas = 5;
+                Console.Write("Nors ir pasirinkai ne ta skaiciu,");
+            }
 
             pirmas[Zenklas - 1].Isvedimas();
 
